71557a9fa60d47f7e3939954f0e31049a31e4868
Maithili sengar <sengarmaithili@gmail.com>
2021-01-12 20:26:26 +0530

    json to csv version 2

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,21 +1,14 @@
-import json
-import csv
+import glob
+import pandas as pd
 
-
-
-with open('/home/user3/Documents/Cdac-Project/Activity/Activity_u00.json') as handle:
-    dictdump = json.loads(handle.read())
-
-print(dictdump)
-
-newdict={}
-for k,v in [(key,d[key]) for d in dictdump for key in d]:
-  if k not in newdict: newdict[k]=[v]
-  else: newdict[k].append(v)
-
-print(newdict)
-
-with open('/home/user3/Documents/Cdac-Project/Activity/mycsvfile.csv', 'w') as f:
-    w = csv.DictWriter(f, newdict.keys())
-    w.writeheader()
-    w.writerow(newdict)
+j = 0
+k = 0
+pathname = '/home/user3/Documents/Cdac-Project/Activity/*.json'
+list_of_paths_to_files = glob.glob(pathname)
+print(list_of_paths_to_files)
+[print(i) for i in list_of_paths_to_files]
+for i in list_of_paths_to_files:
+    df = pd.read_json(i)
+    df.to_csv(f'/home/user3/Documents/Cdac-Project/Activity_to_csv/acivity{i[-7:-5]}.csv', index = None,header=True)
+    j = j+1
+    k = k+1
