4c0b25767f98713900fda7a10a1906ce33e95e2c
Maithili sengar <sengarmaithili@gmail.com>
2021-01-16 12:14:53 +0530

    user_combine_csv version 1

--- csv_combined_csv.py
+++ csv_combined_csv.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import glob
 
-path = r'/home/user3/Documents/Cdac-Project/activity_csv/test'
+path = r'/home/user3/Documents/Cdac-Project/cancelled_class_csv'
 all_files = glob.glob(path + "/*.csv")
+print(all_files)
+
 li = []
 for filename in all_files:
     df = pd.read_csv(filename, index_col=None, header=0, delim_whitespace=True, error_bad_lines=False)
-    li.append(df)
+
+    print(li)
     frame = pd.concat(li, axis=0, ignore_index=True)
 
 
-print(li)
 
-for i in li:
-    try:
-        print(i['working'])
-
-    except KeyError:
-     print(f'The price ofis not known')
+# print(li)
+#
+# for i in li:
+#     try:
+#         print(i['words'])
+#
+#     except KeyError:
+#      print(f'The price ofis not known')
